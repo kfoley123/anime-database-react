b39127157b9b6d56dfe185c955ba73aea1051fd3
Kortney <kortney@iMac.local>
2022-06-14 16:24:17 -0300

    Created state variables and functions to set each variable. Created a handler function for updating the state of each form value. use the handlers with the onchange for each of the form values.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Main() {
+    const [searchBoxText, setSearchBoxText] = useState("");
+    const [rating, setRating] = useState("");
+    const [animeType, setAnimeType] = useState("");
+    const [status, setStatus] = useState("");
+
+    function handleTextBox(event) {
+        setSearchBoxText(event.target.value);
+    }
+    function handleRating(event) {
+        setRating(event.target.value);
+    }
+
+    function handleAnimeType(event) {
+        setAnimeType(event.target.value);
+    }
+
+    function handleStatus(event) {
+        setStatus(event.target.value);
+    }
+
+    function search() {
+        console.log({ searchBoxText, rating, animeType, status });
+    }
+
     return (
         <div>
             <h1>Anime Finder</h1>
             <h2>Search for anime below:</h2>
+
             <div>
                 <form id="searchForm">
                     <input
@@ -12,9 +37,10 @@ export default function Main() {
                         id="search"
                         name="search"
                         placeholder="Search Box"
+                        onChange={handleTextBox}
                     />
-                    <select id="rating" name="rating" value="Rating">
-                        <option selected>Rating</option>
+                    <select id="rating" name="rating" onChange={handleRating}>
+                        <option>Rating</option>
                         <option value="g">G - All Ages</option>
                         <option value="pg">PG - Children</option>
                         <option value="pg13">PG-13 - Teens 13 or older</option>
@@ -25,8 +51,12 @@ export default function Main() {
                         <option value="rx">Rx - Hentai</option>
                     </select>
 
-                    <select id="animeType" name="animeType">
-                        <option selected>Anime Type</option>
+                    <select
+                        id="animeType"
+                        name="animeType"
+                        onChange={handleAnimeType}
+                    >
+                        <option>Anime Type</option>
                         <option value="tv">TV Show</option>
                         <option value="movie">Movie</option>
                         <option value="ova">Ova</option>
@@ -35,15 +65,14 @@ export default function Main() {
                         <option value="music">Music</option>
                     </select>
 
-                    <select id="status" name="status">
-                        <option selected>Status</option>
+                    <select id="status" name="status" onChange={handleStatus}>
+                        <option>Status</option>
                         <option value="airing">Airing</option>
                         <option value="complete">Complete</option>
                         <option value="upcoming">Upcoming</option>
                     </select>
-
-                    <button>Submit</button>
                 </form>
+                <button onClick={search}>Submit</button>
             </div>
         </div>
     );
